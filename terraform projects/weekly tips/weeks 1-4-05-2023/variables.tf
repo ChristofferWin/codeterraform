eb9924103262ef409f2564a5a04c9efe72447f81
ncdmz\cwim <cwim@netcompany.com>
2023-07-07 00:19:14 +0200

    Removing information

--- terraform projects/weekly tips/weeks 1-4-05-2023/variables.tf
+++ terraform projects/weekly tips/weeks 1-4-05-2023/variables.tf
@@ -3,26 +3,22 @@ variable "client_secret" {
   type = string
   sensitive = true
   default = null
-
-  validation {
-    
-  }
 }
 
 variable "subscription_id" {
   description = "the id of the subscription of which to interract with azure"
   type = string
-  default = "07ee07b5-49a4-4519-872a-93e899d90029"
+  default = ""
 }
 
 variable "tenant_id" {
   description = "the id of the tenant of which to interract with azure"
   type = string
-  default = "daa9a3d9-7507-44b4-a06a-805e8e0fee0b"
+  default = ""
 }
 
 variable "client_id" {
   description = "the app id of the spn"
   type = string
-  default = "4c4fe759-1ebf-43d3-a617-4df516258feb"
+  default = ""
 }
