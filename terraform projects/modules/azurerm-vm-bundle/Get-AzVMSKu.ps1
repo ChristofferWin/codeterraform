25538a05108a3be209e659c9576e71e21b930b29
ncdmz\cwim <cwim@netcompany.com>
2023-11-01 22:07:12 +0100

    Missing the script

--- terraform projects/modules/azurerm-vm-bundle/Get-AzVMSKu.ps1
+++ terraform projects/modules/azurerm-vm-bundle/Get-AzVMSKu.ps1
@@ -3,7 +3,14 @@ param (
     [string]$OS,
     [string]$VMPattern,
     [string]$OSPattern,
-    [string]$OutputFileName = ".\SKUs.json"
+    [string]$OutputFileName = ".\SKUs.json",
+    [switch]$OnlyWithVersion
 )
 
-Get-AzVMSKu -Location $Location -OperatingSystem $OS -NewestSKUs -NewestSKUsVersions -Verbose -VMPattern $VMPattern -OSPattern $OSPattern | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutputFileName -Force
+if($OnlyWithVersion) {
+    Get-AzVMSKu -Location $Location -OperatingSystem $OS -NewestSKUs -NewestSKUsVersions -Verbose -VMPattern $VMPattern -OSPattern $OSPattern | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutputFileName -Force
+}
+else{
+    Get-AzVMSKu -Location $Location -OperatingSystem $OS -NewestSKUs -Verbose -VMPattern $VMPattern -OSPattern $OSPattern | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutputFileName -Force
+    #Get-AzVMSKu -Location $Location -OperatingSystem $OS -NewestSKUs -NewestSKUsVersions -Verbose -VMPattern $VMPattern -OSPattern $OSPattern | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutputFileName -Force
+}
