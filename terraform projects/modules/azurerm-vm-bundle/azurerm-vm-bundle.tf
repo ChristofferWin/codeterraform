9411f6846988c7b5ad2eb8f42b76cb0cfab3e235
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-01-07 02:08:25 +0100

    I commented out the wrong provider

--- terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
+++ terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
@@ -2,26 +2,20 @@ terraform {
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        //version = ">=3.76.0"
+        version = ">=3.76.0"
     }
     null = {
         source = "hashicorp/null"
-        //version = ">=3.2.1"
+        version = ">=3.2.1"
     }
     random = {
         source = "hashicorp/random"
-        //version = ">=3.5.1"
+        version = ">=3.5.1"
     }
     local = {
         source = "hashicorp/local"
-        //version = ">=2.4.0"
+        version = ">=2.4.0"
     }
-  }
-}
-
-provider "azurerm" {
-  features {
-    
   }
 }
 
