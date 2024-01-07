a028574e75fd95f835f78d5808030518188da58c
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-01-07 23:12:05 +0100

    Planned update of the module to version 2.2

--- terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
+++ terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
@@ -98,8 +98,8 @@ locals {
   vnet_object_pre2 = local.vnet_object_pre == null && var.vnet_object != null ? {for each in [var.vnet_object] : each.name => each} : null
   vnet_object_helper = can(values(flatten([for each in [local.vnet_object_pre, local.vnet_object_pre2] : each if each != null])[0])[0]) ? values(flatten([for each in [local.vnet_object_pre, local.vnet_object_pre2] : each if each != null])[0])[0] : null
 
-  subnet_objects_pre = var.subnet_objects != null ? {for each in ([for x, y in range(2) : {
-      name = x == 1 ? "AzureBastionSubnet" : var.subnet_objects[x].name
+  subnet_objects_pre = var.subnet_objects != null ? {for each in ([for x, y in length(var.subnet_objects) : {
+      name = (x == 1 || length(var.subnet_objects) == 1) ? "AzureBastionSubnet" : var.subnet_objects[x].name
       address_prefixes = x == 1 && !can(cidrsubnet(var.subnet_objects[x].address_prefixes[0], 6, 0)) && can(var.subnet_objects[x].address_prefixes) ? ["${split("/", var.subnet_objects[x].address_prefixes[0])[0]}/${split("/", var.subnet_objects[x].address_prefixes[0])[1] - (6 - (32 - split("/", var.subnet_objects[x].address_prefixes[0])[1]))}"] : [var.subnet_objects[x].address_prefixes][0]
       service_endpoints = x != 1 ? ["Microsoft.KeyVault"] : null
     }
