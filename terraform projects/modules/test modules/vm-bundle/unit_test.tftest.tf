b75007a93a70972979e9d6b86244409db3a7dfbc
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-03-02 02:26:40 +0100

    again

--- terraform projects/modules/test modules/vm-bundle/unit_test.tftest.tf
+++ terraform projects/modules/test modules/vm-bundle/unit_test.tftest.tf
@@ -28,26 +28,6 @@ module "unit_test_1_using_existing_resources" {
   rg_id = var.rg_id
   vnet_resource_id = var.vnet_resource_id
   subnet_resource_id = var.subnet_resource_id
-
-  vm_windows_objects = [
-    {
-      name = "test-win-vm"
-      os_name = "windows11"
-    },
-    {
-      name = "test-win-vm2"
-      os_name = "windows10"
-    }
-  ]
-
-  vm_linux_objects = [
-    {
-      name = "test-linux-vm"
-      os_name = "UBUNTU" //Check capslock as well
-    },
-    {
-      name = "test-linux-vm2"
-      os_name = "DeBiAN10" //Check upper + lower case
-    }
-  ]
+  vm_windows_objects = var.vm_windows_objects
+  vm_linux_objects = var.vm_linux_objects
 }
