0cb74087577524e61013f93e6d39a31ce18103f4
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-03-10 21:32:50 +0100

    Syntax error

--- terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
+++ terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
@@ -415,7 +415,7 @@ resource "azurerm_virtual_network" "vnet_object" {
 }
 
 resource "azurerm_subnet" "subnet_object" {
-  for_each             = var.subnet_resource_id == null && var.subnet_bastion_resource_id == null && var.create_bastion ? {for each in [for each in local.subnet_objects : each if each.name == "AzureBastion"] : each.name => each} : var.subnet_objects != null ? local.subnet_objects : {}
+  for_each             = local.subnet_objects
   name                 = each.value.name
   resource_group_name  = local.rg_object.name
   virtual_network_name = can(local.vnet_object_helper.name) ? local.vnet_object_helper.name : split("/", var.vnet_resource_id)[8]
