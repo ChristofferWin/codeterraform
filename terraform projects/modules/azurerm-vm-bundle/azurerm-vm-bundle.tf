f9dd3cb815ea3fa6c2569b36f912d0a8c528b43f
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-03-11 01:04:28 +0100

    Forgot the other side

--- terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
+++ terraform projects/modules/azurerm-vm-bundle/azurerm-vm-bundle.tf
@@ -206,7 +206,7 @@ locals {
 
   nic_objects = { for each in [for x, y in local.merge_objects : {
     name                          = can(local.merge_objects[x].nic.name) ? local.merge_objects[x].nic.name : "${local.merge_objects[x].name}-nic"
-    subnet_id                     = [for each in local.subnet_resource_id : each if !contains(split("/", each), "AzureBastionSubnet")][0]
+    subnet_id                     = local.subnet_return_object != null ? [for each in local.subnet_return_object : each.id if each.name != "AzureBastionSubnet"][0] : [for each in local.subnet_resource_id : each if !contains(split("/", each), "AzureBastionSubnet")][0]
     dns_servers                   = can(local.merge_objects[x].nic.dns_servers) ? local.merge_objects[x].nic.dns_servers : null
     enable_ip_forwarding          = can(local.merge_objects[x].nic.enable_ip_forwarding) ? local.merge_objects[x].nic.enable_ip_forwarding : null
     edge_zone                     = can(local.merge_objects[x].nic.edge_zone) ? local.merge_objects[x].nic.edge_zone : null
