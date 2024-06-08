d3adfad196aa67f1854c4e2944796b6f4d0b6238
christoffer <christoffer@codeterraform.com>
2024-06-09 01:31:45 +0200

    Lets see

--- terraform projects/modules/azurerm-hub-spoke/azurerm-hub-spoke.tf
+++ terraform projects/modules/azurerm-hub-spoke/azurerm-hub-spoke.tf
@@ -47,7 +47,7 @@ locals {
   name_fix_pre = local.tp_object.name_prefix != null ? true : false
   name_fix = local.name_fix_pre ? local.name_fix_pre : local.tp_object.name_suffix != null ? false : false
   base_name = local.name_fix == null ? null : local.name_fix && local.tp_object.env_name != null ? "${local.tp_object.name_prefix}-${local.customer_name}-open-${local.env_name}" : local.name_fix == false && local.tp_object.env_name != null ? "${local.env_name}-${local.customer_name}-open-${local.tp_object.name_suffix}" : local.name_fix && local.tp_object.env_name == null ? "${local.tp_object.name_prefix}-${local.customer_name}-open" : local.name_fix == false && local.tp_object.env_name == null && local.tp_object.name_suffix != null ? "${local.customer_name}-open-${local.tp_object.name_suffix}" : null
-  rg_name = local.name_fix ? "rg-${replace(local.base_name, "-open", "hub")}" : local.base_name != null ? "${replace(local.base_name, "-open", "hub")}-rg" : "rg-hub"
+  rg_name = local.name_fix ? "rg-${replace(local.base_name, "-open", "-hub")}" : local.base_name != null ? "${replace(local.base_name, "-open", "hub")}-rg" : "rg-hub"
   vnet_base_name = local.name_fix ? "vnet-${replace(local.base_name, "-open", "hub")}" : local.base_name != null ? "${replace(local.base_name, "-open", "hub")}-vnet" : "vnet-hub"
   gateway_base_name = local.name_fix ? "gw-${replace(local.base_name, "-open", "hub")}" : local.base_name != null ? "${replace(local.base_name, "-open", "hub")}-gw" : "gw-hub-p2s"
   pip_count = local.create_firewall && local.create_vpn ? 2 : local.create_firewall || local.create_vpn ? 1 : 0
@@ -259,7 +259,7 @@ data "azurerm_client_config" "context_object"{
 
 resource "azurerm_resource_group" "rg_object" {
   for_each = local.rg_objects
-  name = length(regexall(each.key, "hub-")) > 0 ? replace(each.key, "hub-", "-hub-") : length(regexall(each.key, "spoke-")) > 0 ? replace(each.key, "spoke-", "-spoke-") : each.key
+  name = each.value.solution_name == null ? each.key : replace(each.key, "spoke", "${each.value.solution_name}-spoke")
   location = each.value.location
 }
 
