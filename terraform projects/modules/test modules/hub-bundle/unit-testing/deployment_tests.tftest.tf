dd177fed0ac63de11514184d88ec3259049e0b5f
christoffer <christoffer@codeterraform.com>
2024-06-18 01:18:05 +0200

    Fix test

--- terraform projects/modules/test modules/hub-bundle/unit-testing/deployment_tests.tftest.tf
+++ terraform projects/modules/test modules/hub-bundle/unit-testing/deployment_tests.tftest.tf
@@ -5,25 +5,25 @@ provider "azurerm" {
 
 module "deployment_1_simple_hub_spoke" {
   source = "github.com/ChristofferWin/codeterraform//terraform projects/modules/azurerm-hub-spoke?ref=main"
-  typology_object = var.deployment_1_simple_hub_spoke
+  topology = var.deployment_1_simple_hub_spoke
 }
 
 module "deployment_2_simple_with_vpn" {
   source = "github.com/ChristofferWin/codeterraform//terraform projects/modules/azurerm-hub-spoke?ref=main"
-  typology_object = var.deployment_2_simple_with_vpn
+  topology = var.deployment_2_simple_with_vpn
 }
 
 module "deployment_3_simple_with_firewall" {
   source = "github.com/ChristofferWin/codeterraform//terraform projects/modules/azurerm-hub-spoke?ref=main"
-  typology_object = var.deployment_3_simple_with_firewall
+  topology = var.deployment_3_simple_with_firewall
 }
 
 module "deployment_4_advanced_with_all_custom_values" {
   source = "github.com/ChristofferWin/codeterraform//terraform projects/modules/azurerm-hub-spoke?ref=main"
-  typology_object = var.deployment_4_mixed_settings
+  topology = var.deployment_4_mixed_settings
 }
 
 module "deployment_5_advanced_with_all_custom_values" {
   source = "github.com/ChristofferWin/codeterraform//terraform projects/modules/azurerm-hub-spoke?ref=main"
-  typology_object = var.deployment_5_advanced_with_all_custom_values
+  topology = var.deployment_5_advanced_with_all_custom_values
 }
