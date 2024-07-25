5400a427ae1dd2a7423a060cdba5c1482b84c0f4
christoffer <christoffer@codeterraform.com>
2024-07-25 18:16:55 +0200

    First upload of go

---
+++ golang projects/first project/src/go.mod
@@ -0,0 +1,17 @@
+module github.com/ChristofferWin/codeterraform
+
+go 1.22.5
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20240721024200-dac8efcb39ce // indirect
+	github.com/chromedp/chromedp v0.9.5 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/valyala/fastjson v1.6.4 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+)
---
+++ golang projects/first project/src/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"github.com/valyala/fastjson"
+)
+
+type ArmBasicObject struct {
+	name          string
+	resource_type string
+	resource_id   string
+}
+
+type AzureRm struct {
+}
+
+// Define the HtmlObject struct with a named attributes field
+type HtmlObject struct {
+	name       string
+	attributes struct {
+		optional []string
+		required []string
+	}
+}
+
+var HtmlObjects = []HtmlObject{}
+var armBasicObjects = []ArmBasicObject{}
+var rawHtmlArray []string
+
+func main() {
+
+	// Define and parse the filepath flag
+	filepath := flag.String("filepath", "", "Path to the file")
+	flag.Parse()
+
+	// Call the ImportArmFile function with the filepath argument
+	fileContent, err := ImportArmFile(filepath)
+
+	if err != nil {
+		// Handle the error if it occurs
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	if err := VerifyArmFile(fileContent); err != nil {
+		fmt.Println("Error validating json file:", err)
+		return
+	}
+
+	baseArmResources, err := RetrieveArmBaseInformation(fileContent)
+
+	for x := 0; x < len(baseArmResources); x++ {
+		rawHtml, err := RetrieveRawHtml(baseArmResources[x].resource_type)
+
+		if err != nil {
+			fmt.Println("Error while trying to retrieve required documentation", err, baseArmResources[x].resource_type)
+			break
+		}
+		cleanHtml := SortRawHtml(rawHtml, baseArmResources[x].resource_type)
+		HtmlObjects = append(HtmlObjects, cleanHtml)
+	}
+
+	fmt.Println(HtmlObjects[0].attributes.required)
+}
+
+func ImportArmFile(filepath *string) ([]byte, error) {
+	file, err := os.ReadFile(*filepath)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+func VerifyArmFile(filecontent []byte) error {
+	err := fastjson.ValidateBytes(filecontent)
+	return err
+}
+
+func RetrieveArmBaseInformation(filecontent []byte) ([]ArmBasicObject, error) {
+	var parserObject fastjson.Parser
+	rawJsonBytes, err := parserObject.ParseBytes(filecontent) //Error overwritten by error 2
+
+	var arrayJsonObjects []*fastjson.Value
+
+	// Check if the parsed JSON is an array or an object
+	if rawJsonBytes.Type() == fastjson.TypeArray {
+		// If it's an array, get the array elements
+		arrayJsonObjects = rawJsonBytes.GetArray()
+		for x := 0; x < len(arrayJsonObjects); x++ {
+			object := ArmBasicObject{
+				name:          string(arrayJsonObjects[x].GetStringBytes("name")),
+				resource_type: string(arrayJsonObjects[x].GetStringBytes("type")),
+				resource_id:   string(arrayJsonObjects[x].GetStringBytes("id")),
+			}
+
+			armBasicObjects = append(armBasicObjects, object)
+		}
+	} else if rawJsonBytes.Type() == fastjson.TypeObject {
+		// If it's an object, convert the object to an array of values
+		object_pre := rawJsonBytes.GetObject()
+		object := ArmBasicObject{
+			name:          string(object_pre.Get("name").GetStringBytes()),
+			resource_type: string(object_pre.Get("type").GetStringBytes()),
+			resource_id:   string(object_pre.Get("id").GetStringBytes()),
+		}
+
+		armBasicObjects = append(armBasicObjects, object)
+	} else {
+		return nil, fmt.Errorf("unexpected JSON type: %s", rawJsonBytes.Type())
+	}
+
+	return armBasicObjects, err
+}
+
+func RetrieveRawHtml(resourceType string) (string, error) {
+	var HtmlBody string
+	var HtmlBodyCompare string
+
+	// Use a regex to find places where we need to insert underscores
+	regex := regexp.MustCompile("([a-z0-9])([A-Z])")
+	convertResourceTypeName := regex.ReplaceAllString(resourceType, "${1}_${2}")
+
+	// Remove the trailing 's' if it exists
+	convertResourceTypeName = strings.TrimSuffix(convertResourceTypeName, "s")
+
+	//Only use right side part
+	convertResourceTypeName = strings.Split(convertResourceTypeName, "/")[1]
+
+	// Convert the entire string to lower case
+	convertResourceTypeName = strings.ToLower(convertResourceTypeName)
+
+	// Create context
+	ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
+	defer cancel()
+
+	// Create a new Chrome instance
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true), // Set to false if you want to see the browser UI
+		chromedp.Flag("disable-gpu", true),
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(ctx, opts...)
+	defer cancel()
+
+	taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	url := fmt.Sprintf("https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/%s", convertResourceTypeName)
+
+	fmt.Println("This is the url:", url)
+
+	for x := 1; x < 25; x++ {
+		err := chromedp.Run(taskCtx,
+			chromedp.Navigate(string(url)),
+			chromedp.WaitReady("body"),                   // Wait for the body to be ready
+			chromedp.Sleep(time.Duration(x)*time.Second), // Give some time for JS to execute
+			chromedp.InnerHTML("*", &HtmlBody, chromedp.ByQueryAll),
+		)
+
+		if err != nil {
+			return "", err
+		}
+
+		if bytes.Equal([]byte(HtmlBody), []byte(HtmlBodyCompare)) {
+			break
+		}
+
+		HtmlBodyCompare = HtmlBody
+	}
+	return HtmlBodyCompare, nil
+}
+
+func SortRawHtml(rawHtml string, resourceType string) HtmlObject { //See the struct type definitions towards the top of the file
+	var requiredArguments []string
+	var optionalArguments []string
+
+	// Define the regular expressions for (Required) and (Optional)
+	requiredRegex := regexp.MustCompile(`\(Required\)`)
+	optionalRegex := regexp.MustCompile(`\(Optional\)`)
+
+	// Filter lines containing (Required) or (Optional)
+	linesHtml := strings.Split(rawHtml, "\n")
+	for _, line := range linesHtml {
+		if requiredRegex.MatchString(line) {
+			requiredArguments = append(requiredArguments, line)
+		} else if optionalRegex.MatchString(line) {
+			optionalArguments = append(optionalArguments, line)
+		}
+	}
+
+	object := HtmlObject{
+		name: resourceType,
+
+		attributes: struct {
+			optional []string
+			required []string
+		}{optionalArguments, requiredArguments},
+	}
+
+	return object
+}
